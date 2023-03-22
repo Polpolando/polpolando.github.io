8ccb2c8b02c77f946c64b1b2731afef1f27e1516
Polpolando <m.k.polando@protonmail.com>
2023-03-22 22:12:01 +0100

    Input fields maker, character class

--- fun/limbus_fight_calculator/script.js
+++ fun/limbus_fight_calculator/script.js
@@ -11,11 +11,29 @@ class Character {
 }
 
 const calculateOdds = (clasher1, clasher2) => {
+   console.log("nic");
    // TODO
+}
+
+const addInputs = DOMElement => {
+   const DOMElementId = DOMElement.id;
+   const inputDataTypes = ["baseValue", "coinValue", "coinAmount", "sanity"];
+   
+   inputDataTypes.forEach(dataType => {
+      inputField = createNumberInput(DOMElementId, dataType);
+      DOMElement.appendChild(inputField);
+   });
+}
+
+const createNumberInput = (id, dataType) => {
+   const DOMElement = document.createElement("input");
+   DOMElement.id = id + "-" + dataType // id-dataType
+   DOMElement.class = "numberInput";
+   DOMElement.placeholder = dataType;
+   return DOMElement;
 }
 
 player = new Character(10, 2, 2, 0);
 enemy = new Character(10, 2, 2, 0);
 
-
-
+addInputs(document.getElementById("player"));
